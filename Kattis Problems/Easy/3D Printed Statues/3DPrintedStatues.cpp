7cd37e71a2281cc4d73c5e811f2f71ce98e680fc
Bryan Ho <hutizaki@gmail.com>
2026-01-31 00:17:38 -0600

    3D PRINTED STATUES - Uploading code before AI usage

---
+++ Kattis Problems/Easy/3D Printed Statues/3DPrintedStatues.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+/*
+    URL: https://open.kattis.com/problems/3dprinter
+    
+    Group Members:
+    - Bryan
+
+    Time to completion:
+    00:39:02 only passed 2 tests
+    Going to AI for help
+*/
+
+/*
+    You can either use a 3D printer to print a statue, or another 3D printer
+
+    Each print takes 1 day
+
+
+
+    n = 5
+
+    only 1 printer means 5 days
+
+    or day 1 make one
+    day 2 you can do 2 at a time
+
+    num print    day     remaining
+        1         1         5
+        2         2         5
+        2         3         3
+        2         4         1
+        2         5         0  
+
+
+    Num printers = num statues per day
+
+    5 4 3 2 1   -1
+    5 5 3 1 0   -2
+    5 5 5 1 0   -4
+
+
+    6 5 4 3 2 1     -1
+    6 6 4 2 0 0     -2
+    6 6 6 2 0 0     -4
+
+
+    7 6 5 4 3 2 1    -1
+    7 7 5 3 1 0 0    -2   
+    7 7 7 3 0 0 0    -4
+
+
+    8 7 6 5 4 3 2 1
+    8 8 6 4 2 0
+    8 8 8 4 0
+    8 8 8 8 0
+    Brute force approach, try each iteration, but then double the subtraction (num printers doubles per day) and find the min num days
+*/
+
+
+/*
+    AI Help:
+    
+*/
+
+
+
+int main() {
+    int numStatues;
+
+    cin >> numStatues;
+
+    vector<int> days;
+
+    for (int daysUsedForPrinter = 0; daysUsedForPrinter < numStatues; daysUsedForPrinter++) {
+        int remainingStatues = numStatues;
+        int printCapacity = daysUsedForPrinter == 0 ? 1 : (daysUsedForPrinter * 2);
+        int numDays = daysUsedForPrinter;
+        while (remainingStatues > 0) {
+            remainingStatues -= printCapacity;
+            numDays++;
+        }
+
+        days.push_back(numDays);
+    }
+
+    auto min_ptr = min_element(days.begin(), days.end());
+    int min_value = *min_ptr;
+
+    cout << min_value;
+    return 0;
+}
